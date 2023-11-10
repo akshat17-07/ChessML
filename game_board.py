c03781622650953ca906caf1f8477c3f061fd074
Akshat <akshatharlalka.ah@icloud.com>
2023-11-10 14:07:13 -0500

    Chess Board created

--- game_board.py
+++ game_board.py
@@ -3,8 +3,18 @@ from sys import exit
 
 # initializing pygame settings
 pygame.init()
-screen = pygame.display.set_mode((1200, 800))
+board_size = (1200, 800)
+screen = pygame.display.set_mode(board_size)
 pygame.display.set_caption("Chess")
+screen.fill("grey")
+
+# creating surface for white and black squares
+offset = int(min(board_size[0]/90/8, board_size[1]/90/8)) + 20
+length = int(min(board_size[0]/8 - board_size[0]/90/8,board_size[1]/8 - board_size[1]/90/8))
+block_size = pygame.Surface((length, length))
+WHITE = (0, 0, 0)
+BLACK = (255, 255, 255)
+
 
 # creating clock object
 clock = pygame.time.Clock()
@@ -17,6 +27,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+    flag = True
+    for i in range(8):
+        flag = not flag
+        for j in range(8):
+            flag = not flag
+            if flag:
+                pygame.draw.rect(screen, WHITE, ((i*length)+offset,(j*length)+offset, length, length))
+            else:
+                pygame.draw.rect(screen, BLACK, ((i*length)+offset,(j*length)+offset, length, length))
+            
     pygame.display.update()
     clock.tick(60)
