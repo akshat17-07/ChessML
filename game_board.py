2e0f2c90405baf3e055655050180fb472044d72c
Akshat <akshatharlalka.ah@icloud.com>
2023-11-14 14:01:35 -0500

    pieces could move anywhere on the board

--- game_board.py
+++ game_board.py
@@ -1,6 +1,7 @@
 from sys import exit
 import board_creator
 import pygame
+import time
 
 # initializing pygame settings
 pygame.init()
@@ -22,7 +23,7 @@ Board = board_creator.reset_board()
 
 # creating clock object
 clock = pygame.time.Clock()
-
+selected = None
 # running the game
 while True:
     for event in pygame.event.get():
@@ -30,6 +31,28 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+        # checking for moves
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Get the position of the mouse click
+            x, y = pygame.mouse.get_pos()
+            x_pos = (x-offset)//(length)
+            y_pos = (y-offset)//(length)
+
+            # checking if the position is valid
+            if y_pos > 7 or y_pos<0 or x_pos<0 or x_pos > 7:
+                selected = None
+
+            # checking if selected
+            elif not selected:
+                selected = str(y_pos) + str(x_pos)
+            
+            # checking if not selected
+            else:
+                temp = str(y_pos) + str(x_pos)
+                Board[temp], Board[selected] = Board[selected], None
+                selected = None
+
     flag = True
     for i in range(8):
         flag = not flag
@@ -39,6 +62,8 @@ while True:
                 pygame.draw.rect(screen, GREEN, ((i*length)+offset,(j*length)+offset, length, length))
             else:
                 pygame.draw.rect(screen, WHITE, ((i*length)+offset,(j*length)+offset, length, length))
+            if selected == str(j) + str(i):
+                    pygame.draw.rect(screen, pygame.Color(255, 0, 0, 50), ((i*length)+offset,(j*length)+offset, length, length))
             if Board[str(j) + str(i)]:
                 screen.blit(Board[str(j) + str(i)], ((i*length)+2*offset,(j*length)+2*offset))
     pygame.display.update()
